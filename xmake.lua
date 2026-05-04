8676005edfaaeb5626ae707ae613d220db4b41c9
LYJ <532874387@qq.com>
2026-05-05 01:15:38 +0800

    add win平台

--- xmake.lua
+++ xmake.lua
@@ -1,6 +1,11 @@
 add_rules("mode.debug", "mode.release")
+set_encodings("utf-8")
+-- set_languages("cxx23")
+set_languages("cxxlatest")
 
-set_languages("cxx23")
+-- if is_plat("windows") then
+--     add_cxxflags("/utf-8")
+-- end
 
 add_requires("spdlog")
 
@@ -10,12 +15,20 @@ target("pinyin")
     add_includedirs("include")
     add_linkdirs("lib")
     add_links("ib_pinyin_c")
-    add_rpathdirs("$ORIGIN")
+    if is_plat("linux") then
+        add_rpathdirs("$ORIGIN")
+    elseif is_plat("windows") then
+        add_links("ntdll")
+    end
     add_files("src/main.cpp", "src/pinyin.cpp")
     after_build(function (target)
-        os.cp("lib/*.so", target:targetdir())
+        if is_plat("linux") then
+            os.cp("lib/libib_pinyin_c.so", target:targetdir())
+        elseif is_plat("windows") then
+            os.cp("lib/ib_pinyin_c.dll", target:targetdir())
+        end
     end)
-    --
+
 --https://github.com/Chaoses-Ib/ib-matcher.git
 target("cli")
     set_kind("binary")
@@ -23,8 +36,16 @@ target("cli")
     add_includedirs("include")
     add_linkdirs("lib")
     add_links("ib_pinyin_c")
-    add_rpathdirs("$ORIGIN")
+    if is_plat("linux") then
+        add_rpathdirs("$ORIGIN")
+    elseif is_plat("windows") then
+        add_links("ntdll")
+    end
     add_files("src/cli.cpp", "src/pinyin.cpp")
     after_build(function (target)
-        os.cp("lib/*.so", target:targetdir())
+        if is_plat("linux") then
+            os.cp("lib/libib_pinyin_c.so", target:targetdir())
+        elseif is_plat("windows") then
+            os.cp("lib/ib_pinyin_c.dll", target:targetdir())
+        end
     end)
